b1259219c9744ea066ceef6fc415957d0d5b7bba
Graze <82724623+The-Graze@users.noreply.github.com>
2024-11-10 07:41:38 +0000

    I have not a fucking clue mate

--- Graze.BaitRefill/BaitMod.cs
+++ Graze.BaitRefill/BaitMod.cs
@@ -7,15 +7,21 @@ namespace Graze.BaitRefill;
 public class BaitMod : IScriptMod {
     public bool ShouldRun(string path) => path == "res://Scenes/HUD/BaitButton/bait_showcase.gd";
 
+    string selectedBait;
+
     public IEnumerable<Token> Modify(string path, IEnumerable<Token> tokens) {
         var waiter = new MultiTokenWaiter([
-
+            t => t is IdentifierToken {Name: "bait_selected"}
         ], allowPartialMatch: true);
 
         // loop through all tokens in the script
         foreach (var token in tokens) {
             if (waiter.Check(token)) {
-
+                yield return token;
+                if (token.AssociatedData.HasValue)
+                {
+                    selectedBait = token.AssociatedData.Value.ToString();
+                }
             } else {
 
                 yield return token;
